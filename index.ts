4540e302c1393be1db38ff3701efe634c7805606
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-03-10 18:00:30 +0530

    enabled streamed input

--- index.ts
+++ index.ts
@@ -1,42 +1,43 @@
-import Groq from "groq-sdk"
-import * as readline from "node:readline"
+import Groq from "groq-sdk";
+import * as readline from "node:readline";
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
-})
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-const customInstructions = `Your name is Alex, you are my personal assistant, your job is to answer my questions politely and accurately`
+const customInstructions = "You are a helpful assistant.";
 
 async function main() {
   rl.question("Enter your message: ", async (input) => {
-    const completion = await groq.chat.completions
-      .create({
-        messages: [
-          {
-            role: "system",
-            content: customInstructions,
-          },
-          {
-            role: "user",
-            content: input,
-          },
-        ],
-        model: "mixtral-8x7b-32768",
-      })
-      .then((chatCompletion: any) => {
-        process.stdout.write(chatCompletion.choices[0]?.message?.content || "")
-        rl.close()
-      })
-      .catch((err) => {
-        console.error(err)
-        rl.close()
-      })
-  })
+    const stream = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: customInstructions,
+        },
+        {
+          role: "user",
+          content: input,
+        },
+      ],
+      model: "mixtral-8x7b-32768",
+      temperature: 0.5,
+      max_tokens: 1024,
+      top_p: 1,
+      stream: true,
+    });
+
+    for await (const chunk of stream) {
+      process.stdout.write(chunk.choices[0]?.delta?.content || "");
+    }
+
+    rl.close();
+  });
 }
 
-main()
+main();
